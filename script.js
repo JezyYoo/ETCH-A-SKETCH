9e3674665b95765e2a36a049f6e98dfb719b047d
JezyYoo <volchevdanya@gmail.com>
2022-06-01 17:51:07 +0300

    changing the count of cells added

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let container = document.querySelector('.container');
 let buttonCellChange = document.querySelector('.changeCellCount');
 let tempCell;
+let containerSize = container.clientWidth;
+console.log(containerSize)
+
+// container.style.width = '480px';
+// container.style.height = '480px';
 
 let hovering = function(event){
     event.target.style.backgroundColor = "black";
 }
 let generateField = (count)=>{
+    let cellSize = containerSize/count;
+    console.log(cellSize)
     for (let y= 0; y < count; y++) {
         for (let x = 0; x < count; x++) {
             tempCell = document.createElement('div');
             tempCell.classList.add('cell');
             tempCell.addEventListener('mouseover',hovering)
+            tempCell.style.width = `${cellSize}px`;
+            tempCell.style.height = `${cellSize}px`;
             container.appendChild(tempCell);
             
         }
@@ -26,8 +35,12 @@ let cleanField = ()=>{
 
 buttonCellChange.addEventListener('click', ()=>{    
     let count = prompt('Enter new count of cell per side');
-    console.log(!isNaN(parseFloat(count)))
-    if(!isNaN(parseFloat(count)))
+
+    if(parseFloat(count)>100)
+    {
+        alert("Enter the number less than 100")
+    }
+    else if(!isNaN(parseFloat(count)))
     {
         cleanField();
         generateField(count)
