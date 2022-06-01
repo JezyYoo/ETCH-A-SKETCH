7fbd942ab17f7b37f94d9ca953b4652ad9302795
JezyYoo <volchevdanya@gmail.com>
2022-06-01 17:34:31 +0300

    clearField and button changeCountofcells added with prompt

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
-let container = document.querySelector('.container');;
+let container = document.querySelector('.container');
+let buttonCellChange = document.querySelector('.changeCellCount');
 let tempCell;
 
-let Hovering = function(event){
+let hovering = function(event){
     event.target.style.backgroundColor = "black";
 }
-
-for (let y= 0; y < 16; y++) {
-    for (let x = 0; x < 16; x++) {
-        tempCell = document.createElement('div');
-        tempCell.classList.add('cell');
-        tempCell.addEventListener('mouseover',Hovering)
-        container.appendChild(tempCell);
-        
+let generateField = (count)=>{
+    for (let y= 0; y < count; y++) {
+        for (let x = 0; x < count; x++) {
+            tempCell = document.createElement('div');
+            tempCell.classList.add('cell');
+            tempCell.addEventListener('mouseover',hovering)
+            container.appendChild(tempCell);
+            
+        }
+    
     }
-
 }
+
+let cleanField = ()=>{
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+      }
+}
+
+buttonCellChange.addEventListener('click', ()=>{    
+    let count = prompt('Enter new count of cell per side');
+    console.log(!isNaN(parseFloat(count)))
+    if(!isNaN(parseFloat(count)))
+    {
+        cleanField();
+        generateField(count)
+    }
+    else
+    {
+        alert("Enter only numbers!")
+    }
+})
+
+generateField(16)
+
+
 
